b67ce4249a4c5035c1ae5e5f30ff9a95827cf41e
TimBennetts <tben873@aucklanduni.ac.nz>
2017-05-05 10:42:57 +1200

    Add files via upload

--- createDatabase.py
+++ createDatabase.py
@@ -1,78 +1,121 @@
 import sqlalchemy
 import os
 import csv
+import sqlite3
+# from Classes import TandWPurchaseOrder
+# import Classes; reload(Classes)
 
 from readData import *
 from sqlalchemy import *
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
+
+#Change these
+dbname = "PurchaseOrders"
+
 metadata = MetaData()
 Base = declarative_base()
-filePath = "CAM MIKE DATA\B2B\THE ACTIVE PO"
+filePath = "CAM MIKE DATA\B2B"
+filePath2 = "CAM MIKE DATA\Stock Reports"
 purchDirectory = os.path.abspath(os.path.join(filePath))
+stockDirectory = os.path.abspath(os.path.join(filePath2))
 print purchDirectory
 
+class theActivePurchaseOrder(Base):
+    __tablename__ = 'theActivePurchaseOrders'
 
-def createDB(dbname):
-    dbEngine = create_engine('sqlite:///' + dbname, echo=False)
-
-    Session = sessionmaker(bind=dbEngine)
-    dbSession = Session()
-    Base.metadata.create_all(dbEngine)
-    print "db created"
-    return dbSession
-
-
-dbSession = createDB('bbyoSales.db')
-
-class purchaseOrder(Base):
-    __tablename__ = 'purchaseOrders'
-
+    date = Column(String)
     description = Column(String)
     skuNum = Column(String, primary_key=True)
     eanBarcode = Column(String)
+    quantity = Column(Integer) #or total quantity
+    costPrice = Column(Float) #exclude gst
+    totalCost = Column(Float) #exclude gst
+    gst = Column(Float)
+
+class TandWPurchaseOrder(Base):
+    __tablename__ = 'TandWPurchaseOrder'
+
+    orderNum = Column(String, primary_key=True)
+    barcode = Column(String)
+    skuNum = Column(String)
+    shipID = Column(String)
+    description = Column(String)
     quantity = Column(Integer)
     costPrice = Column(Float)
     totalCost = Column(Float)
-    gst = Column(Float)
+    agreedDeliveryDate = Column(String)
+    colour = Column(String)
+    material = Column(String)
+    itemDims = Column(String)
+    CoO = Column(String) #what even is this?
+    itemWeightEa = Column(String)
+    shipWidth = Column(String)
+    shipDepth = Column(String)
+    shipHeight = Column(String)
+    itemCubic = Column(String)
+    totalCubic = Column(String)
+    deadweight = Column(String)
 
-def readPurchaseOrder(purchDirectory, session):
-    wsList = readFiles(purchDirectory)
-    wsRange = []
-    headers = []
-    # print type(wsList) #List
-    for i,ws in enumerate(wsList):
-        # print type(ws)
-        list1, list2 = readWs(ws)  # list1 = wsRange, list2 = headers
-        wsRange.append(list1)
-        headers.append(list2)
-        # print headers[0]
-        #print type(wsRange)
-        #wsRange[i(ws)][j][k] = k item in row j of table/book i
-        for j, wsRow in enumerate(wsRange):
-            # if len(wsRange[i][j]) < 7:
-            #     mismatch = 7-len(wsRange[i][j])
-            #     while mismatch > 0:
-            #         wsRange[i][j].append(0)
-            #         mismatch = mismatch - 1
+class OZPurchaseOrder(Base):
+    __tablename__ = 'OZPurchaseOrder'
 
-            purchOrders = purchaseOrder(
-                                        description = wsRange[i][j][0].value,
-                                        skuNum = wsRange[i][j][1].value,
-                                        eanBarcode = wsRange[i][j][2].value,
-                                        quantity = wsRange[i][j][3].value,
-                                        costPrice = wsRange[i][j][4].value,
-                                        totalCost = wsRange[i][j][5].value,
-                                        gst = wsRange[i][j][6].value,
-                                        )
-    session.add(purchOrders)
-    session.commit()
-
-readPurchaseOrder(purchDirectory, dbSession)
+    orderNum = Column(String, primary_key=True)
+    itemName = Column(String)
+    skuNum = Column(String)
+    size = Column(String)
+    quantity = Column(Integer)
+    costPrice = Column(Float)
+    totalCost = Column(Float)
+    boxNum = Column(String)
+    shipped = Column(String)
 
 class SalesData(Base):
     __tablename__ = 'SalesData'
+
+    saleID = Column(String, primary_key=True)
+    date = Column(String)
+    orderName = Column(String)
+    transactionType = Column(String)
+    saleType = Column(String)
+    salesChannel = Column(String)
+    POSLocation = Column(String)
+    billingCountry = Column(String)
+    billingRegion = Column(String)
+    billingCity = Column(String)
+    shippingCountry = Column(String)
+    shippingRegion = Column(String)
+    shippingCity = Column(String)
+    productType = Column(String)
+    productVendor = Column(String)
+    productTitle = Column(String)
+    productVariantTitle = Column(String)
+    productVariantTitleSku = Column(String)
+    quantity = Column(Integer)
+    costPrice = Column(Float)
+    lineItemDiscount = Column(Float)
+    orderDiscount = Column(Float)
+    priceAfterDisc = Column(Float)
+    Taxes = Column(Float)
+    finalPrice = Column(Float)
+
+class hardToFind(Base):
+    __tablename__ = 'hardToFind'
+
+    orderID = Column(Integer, primary_key=True)
+    customerName = Column(String)
+    shippingAddress = Column(String)
+    shippingSuburb = Column(String)
+    shippingState = Column(String)
+    shippingPostcode = Column(Integer)
+    shippingCountry = Column(String)
+    orderDate = Column(String)
+    orderStatus = Column(String)
+    shipDate = Column(String)
+
+class ordersExport(Base):
+    __tablename__ = 'ordersExport'
 
     nameID = Column(String, primary_key=True)
     email = Column(String)
@@ -141,77 +184,124 @@ class SalesData(Base):
     tax5Name = Column(String)
     tax5Value = Column(Float)
 
-# def readSalesData(session, cells):
-#     for i in range(len(cells)):
-#         ordersExport = SalesData(
-#             nameID=Column(String, primary_key=True),
-#             email = Column(String)
-#             paymentStatus = Column(String)
-#             timePaid = Column(DateTime)
-#             fulfillmentStatus = Column(String)
-#             fulfillmentTime = Column(DateTime)
-#             acceptMarketing = Column(String)
-#             currency = Column(String)
-#             priceSubtotal = Column(Float)
-#             priceShipping = Column(Float)
-#             priceTaxes = Column(Float)
-#             priceTotal = Column(Float)
-#             discountCode = Column(String)
-#             discountAmount = Column(Float)
-#             shippingMethod = Column(String)
-#             timeEntryCreated = Column(DateTime)
-#             lineitemQty = Column(Integer)
-#             lineitemName = Column(String)
-#             lineitemPrice = Column(Float)
-#             lineitemComparePrice = Column(Float)  # what is this?
-#             lineitemSku = Column(String)
-#             lineitemReqShipping = Column(Boolean)
-#             lineitemTaxable = Column(Boolean)
-#             lineitemFulfillmentStatus = Column(String)
-#             billingName = Column(String)
-#             billingStreet = Column(String)
-#             billingAddress1 = Column(String)
-#             billingAddress2 = Column(String)
-#             billingCompany = Column(String)
-#             billingCity = Column(String)
-#             billingZip = Column(String)
-#             billingProvince = Column(String)
-#             billingCountry = Column(String)
-#             billingPhone = Column(String)
-#             shippingName = Column(String)
-#             shippingStreet = Column(String)
-#             shippingAddress1 = Column(String)
-#             shippingAddress2 = Column(String)
-#             shippingCompany = Column(String)
-#             shippingCity = Column(String)
-#             shippingZip = Column(String)
-#             shippingProvince = Column(String)
-#             shippingCountry = Column(String)
-#             shippingPhone = Column(String)
-#             notes = Column(String)
-#             noteAttributes = Column(String)
-#             timeCancelled = Column(DateTime)
-#             paymentMethod = Column(String)
-#             paymentRef = Column(String)
-#             refundAmount = Column(String)
-#             vendor = Column(String)
-#             ID = Column(Integer)
-#             tags = Column(String)
-#             riskLevel = Column(String)
-#             source = Column(String)
-#             lineitemDiscount = Column(Float)
-#             tax1Name = Column(String)
-#             tax1Value = Column(Float)
-#             tax2Name = Column(String)
-#             tax2Value = Column(Float)
-#             tax3Name = Column(String)
-#             tax3Value = Column(Float)
-#             tax4Name = Column(String)
-#             tax4Value = Column(Float)
-#             tax5Name = Column(String)
-#             tax5Value = Column(Float)
-#         )
-#
+class stockReports(Base):
+    __tablename__ = 'StockReports'
+    itemNum = Column(String, primary_key=True)
+    skuNum = Column(String)
+    description = Column(String)
+    receipts = Column(Integer)
+    picks = Column(Integer)
+    frozenStock = Column(Integer)
+    freeStock = Column(Integer)
 
-    # session.add(ordersExport)
-    # session.commit()
+
+
+
+def createDB(dbname):
+    global dbEngine
+    global dbSession
+
+    dbEngine = create_engine('sqlite:///' + dbname, echo=False)
+    #create Session
+    Session = sessionmaker()
+    Session.configure(bind=dbEngine)
+    dbSession = Session()
+
+    #create tables
+    Base.metadata.create_all(dbEngine)
+    print "db created"
+
+
+def readPurchaseOrder(purchDirectory, session):
+    wsList = readFiles(purchDirectory)
+    print purchDirectory
+    wsRange = []
+    headers = []
+    for ws in wsList:
+        list1, list2 = readWs(ws)  # list1 = wsRange, list2 = headers
+        wsRange.append(list1)
+        headers.append(list2)
+
+    if "OZSALE" in purchDirectory:
+        wsRange = refineRange(wsRange, headers) #ToDo
+
+    counter = 1
+    for i in range(len(wsRange)):
+        for j in range(len(wsRange[i])):
+            if "OZSALE" in purchDirectory:
+                OZPurchOrder = OZPurchaseOrder(
+                    orderNum = counter,
+                    itemName=Column(String),
+                    skuNum = Column(String, primary_key=True),
+                    quantity = Column(Integer),
+                    costPrice = Column(Float),
+                    totalCost = Column(Float),
+                )
+                dbSession.add(OZPurchOrder)
+
+            elif "TEMPLE AND WEBSTER" in purchDirectory:
+                TandWPurchOrder = TandWPurchaseOrder(
+                                                    orderNum = counter,
+                                                    barcode = wsRange[i][j][1].value,
+                                                    skuNum = wsRange[i][j][2].value,
+                                                    shipID = wsRange[i][j][3].value,
+                                                    description = wsRange[i][j][4].value,
+                                                    quantity = wsRange[i][j][5].value,
+                                                    costPrice = wsRange[i][j][6].value,
+                                                    totalCost = wsRange[i][j][7].value,
+                                                    agreedDeliveryDate = wsRange[i][j][8].value,
+                                                    colour = wsRange[i][j][9].value,
+                                                    material = wsRange[i][j][10].value,
+                                                    itemDims = wsRange[i][j][11].value,
+                                                    CoO = wsRange[i][j][12].value,  # what even is this?
+                                                    itemWeightEa = wsRange[i][j][13].value,
+                                                    shipWidth = wsRange[i][j][14].value,
+                                                    shipDepth = wsRange[i][j][15].value,
+                                                    shipHeight = wsRange[i][j][16].value,
+                                                    itemCubic = wsRange[i][j][17].value,
+                                                    totalCubic = wsRange[i][j][18].value,
+                                                    deadweight = wsRange[i][j][19].value,
+                                                    )
+                dbSession.add(TandWPurchOrder)
+            elif "THE ACTIVE" in purchDirectory:
+                theActivePurchOrder = theActivePurchaseOrder(
+                   #ToDO
+                )
+                dbSession.add(theActivePurchOrder)
+            counter = counter + 1
+    dbSession.commit()
+
+def readStockReports(stockDirectory):
+    wsList = readFiles(stockDirectory)
+    print stockDirectory
+    wsRange =[]
+    headers =[]
+    for ws in wsList:
+        list1, list2 = readStockWs(ws)  # list1 = wsRange, list2 = headers
+        wsRange.append(list1)
+        headers.append(list2)
+    counter1 = 0
+    for i in range(len(wsRange)):
+        for j in range(len(wsRange[i])):
+            stockReport = stockReports(
+                itemNum = counter1,
+                skuNum = wsRange[i][j][0].value,
+                description = wsRange[i][j][1].value,
+                receipts = wsRange[i][j][2].value,
+                picks = wsRange[i][j][3].value,
+                frozenStock = wsRange[i][j][4].value,
+                freeStock = wsRange[i][j][5].value,
+                )
+            dbSession.add(stockReport)
+            counter1 = counter1 + 1
+    dbSession.commit()
+
+createDB(dbname)
+Base.metadata.create_all(dbEngine)
+# for subDirs, dirs, files in os.walk(purchDirectory):
+#     if subDirs != purchDirectory:
+#         # print subDirs
+#         readPurchaseOrder(subDirs, dbSession)
+readStockReports(stockDirectory)
+
+
--- readData.py
+++ readData.py
@@ -16,27 +16,57 @@ def getCellRange(start_col, start_row, end_col, end_row, ws):
 def recordHeaders(startRow, endCol, ws):
     headers = []
     counter = 0
+    # print ws.cell(startRow-1, counter)
+    # print startRow -1
+    # print counter
     while counter < endCol:
-        headers.append(ws.cell(startRow-1, counter))
+        if ws.cell(startRow-1, counter).value == "":
+            headers.append("empty")
+        else:
+            headers.append(ws.cell(startRow-1, counter))
         counter = counter + 1
     return headers
 
 def readWs(ws):
     #inputs = worksheet
     startCol= 0
-    endCol = ws.ncols
-    #endCol = 7
+    startRow = 0
     #finding row range
     for i in range(0, ws.nrows):
-        if ws.cell(i, 0).value == "DESCRIPTION":
-            startRow = i + 1
-        elif ws.cell(i, 0).value == "TOTAL":
-            endRow = i - 1
-    wsRange = getCellRange(startCol, startRow, 6, endRow, ws)
+        #if ws.cell(i, 0).value == "DESCRIPTION":
+        # if ws.cell(i,2).value == "Supplier SKU": #need to find way to adapt this to all PO's
+        if type(ws.cell(i,2).value) == unicode and type(ws.cell(i,0).value) == unicode and type(ws.cell(i,1).value) == unicode: #Below line does not like non-unicode types
+            if "SKU" in ws.cell(i,2).value or "Item Name" in ws.cell(i,2).value: #hard-coding
+                startRow = i + 1
+                useCol = 2
+            elif "SKU" in ws.cell(i,1).value or "Style Name" in ws.cell(i,1).value:
+                startRow = i + 1
+                useCol = 1
+            elif "Item Name" in ws.cell(i,0).value:
+                startRow = i + 1
+                useCol = 0
+
+        if startRow != 0:
+            if ws.cell(i, useCol).value == "":
+                endRow = i - 1
+                break
+            elif i == ws.nrows-1:
+                endRow = i
+
+    #finding col range
+    for j in range(0, ws.ncols):
+        # print ws.ncols
+        #print ws.cell(startRow - 1, j).value
+        if ws.cell(startRow-1, j).value == "":
+            endCol = j - 1
+            break
+        elif j == ws.ncols - 1:
+            endCol = j
+
+    wsRange = getCellRange(startCol, startRow, endCol, endRow, ws)
     headers = recordHeaders(startRow, endCol, ws) #list of headers
 
     return wsRange, headers
-
 
 def readFiles(directory):
     i = 0
@@ -44,51 +74,55 @@ def readFiles(directory):
     #importing all files in directory
     for root,dirs,files in os.walk(directory):
         for file in files:
-            if file.endswith(".xlsx"):
+            if file.endswith(".xlsx") or file.endswith(".xls"):
                 # print file
                 fin = xlrd.open_workbook(os.path.abspath(os.path.join(directory,file)))
-                # print file
-
                 #grab first worksheet
                 wsList.append(fin.sheet_by_index(0))
+    return wsList
 
-                #= fin.sheet_by_index(0)
-                #wsRange = readWs(ws)
-                #print wsRange
-    #return wsRange
-    # print ws
-    return(wsList)
+def refineRange(wsRange, headers):
+    wsRangeNew = []
+    for i, header in enumerate(headers):
+        for j, title in enumerate(header):
+            # print i, title
+            if title.value == "Item Name":
+                print wsRange[i][j]
+              # wsRangeNew[i][0] = wsRange[i][j]
+            elif "SKU" in title.value:
+                print i, j
 
-# class purchaseOrder(Base):
-#     __tablename__ = 'purchaseOrders'
-#
-#     description = Column(String)
-#     skuNum = Column(String, primary_key=True)
-#     eanBarcode = Column(String)
-#     quantity = Column(Integer)
-#     costPrice = Column(Float)
-#     totalCost = Column(Float)
-#     gst = Column(Float)
-#
-#
-# filePath = "CAM MIKE DATA\B2B\THE ACTIVE PO"
-# directory = os.path.abspath(os.path.join(filePath))
-# print directory
-# wsRange = []
-# headers = []
-# wsList = readFiles(directory)
-#     # print type(wsList) #List
-# for i,ws in enumerate(wsList):
-#     #print i
-#     list1, list2 = readWs(ws) #list1 = wsRange, list2 = headers
-#     wsRange.append(list1)
-#     headers.append(list2)
-# purchaseOrders = ["DESCRIPTION", "SKU_Number", "Quantity_Cost", "Price_EXC_GST", "Total_Cost_EXC_GST"]
-#
-# print type(wsRange[0][0][4].value)
-# print type(wsRange[0][0][4])
-# wsRange[0][0].append(0)
-# print type(wsRange[0][0][5].value)
+
+    return wsRange
+
+def readStockWs(ws):
+    startRow = 0
+    startCol = 0
+    endRow = 0
+    #finding row range
+    for i in range(0, ws.nrows):
+        if  type(ws.cell(i,0).value) == unicode:
+            if "Item No." in ws.cell(i,0).value and i < ws.nrows-2:
+                if "Owner" in ws.cell(i+1,0).value:
+                    startRow = i+2
+                elif startRow == 0:
+                    startRow = i+1
+            elif "Total for Category" in ws.cell(i,0).value:
+                endRow = i - 1
+            elif "Items" in ws.cell(i,0).value and endRow == 0:
+                endRow = i - 1
+    #finding col range
+    for j in range(0, ws.ncols):
+        if ws.cell(startRow,j).value == "":
+            endCol = j - 1
+        elif j == ws.ncols - 1:
+            endCol = j
+
+    wsRange = getCellRange(startCol, startRow, endCol, endRow, ws)
+    headers = recordHeaders(startRow, endCol, ws)
+    return wsRange, headers
+
+
 
 
 
